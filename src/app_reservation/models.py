270af1f0e8119a5558cbb9415fce72e66b0e1875
Lucas Soria <soria.lucas@hotmail.com>
2025-05-14 23:07:36 -0300

    detalles en lectura

--- src/app_reservation/models.py
+++ src/app_reservation/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from app_clients.models import Clients
+from app_services.models import Service
+from employees_app.models import Employee
+from app_coordinators.models import Coordinator
 
 
 class Reservation(models.Model):
     reservation_date = models.DateField(auto_now_add=True, verbose_name="Reservation Date")
     service_date = models.DateTimeField(verbose_name="Service Date")
-    client = models.ForeignKey('app_clients.Clients', on_delete=models.CASCADE, verbose_name="Client")
-    service = models.ForeignKey('app_services.Service', on_delete=models.CASCADE, verbose_name="Service")
-    employee = models.ForeignKey('employees_app.Employee', on_delete=models.CASCADE, verbose_name="Employee", related_name='reservations_as_employee')
-    coordinator = models.ForeignKey('app_coordinators.Coordinator', on_delete=models.CASCADE, verbose_name="Coordinator", related_name='reservations_as_coordinator')
+    client = models.ForeignKey(Clients, on_delete=models.CASCADE, verbose_name="Client")
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name="Service")
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name="Employee", related_name='reservations_as_employee')
+    coordinator = models.ForeignKey(Coordinator, on_delete=models.CASCADE, verbose_name="Coordinator", related_name='reservations_as_coordinator')
 
     def __str__(self):
         return f"Reservation for {self.client} on {self.service_date} - Service: {self.service.name}"
