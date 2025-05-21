f3ba7ecfbcb9fe0c3332c36455c0754300f816b5
Lucas Soria <soria.lucas@hotmail.com>
2025-05-20 21:20:34 -0300

    views update y list de Service, rutas y templates

--- src/app_services/views.py
+++ src/app_services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.urls import reverse_lazy
 from .models import Service
 
@@ -8,10 +8,26 @@ class ServiceCreateView(CreateView):
     model = Service
     template_name = 'app_services/service_form.html'
     fields = ['name', 'description', 'price']
-    # success_url = reverse_lazy('app_services:service_list') 
+    success_url = reverse_lazy('app_services:services_list') 
 
 
 class ServiceDetailView(DetailView):
     model = Service
     template_name = 'app_services/service_detail.html'
+    context_object_name = 'service'
+
+
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'app_services/services_list.html'
+    context_object_name = 'services'
+    paginate_by = 10 # Number of services per page
+    ordering = ['name']
+
+
+class ServiceUpdateView(UpdateView):
+    model = Service
+    template_name = 'app_services/service_form.html'
+    fields = ['name', 'description', 'price']
+    success_url = reverse_lazy('app_services:services_list') 
     context_object_name = 'service'
