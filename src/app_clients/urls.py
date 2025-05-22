74c0fd99bfcf5f774824a7987825175eba368c32
leanr264 <leandroracedo2604@gmail.com>
2025-05-22 20:16:13 -0300

    delete view de cliente, ruta, agregados en templates detail y list

--- src/app_clients/urls.py
+++ src/app_clients/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (ClientsCreateView, ClientsListView,
-                    ClientsDetailView, ClientsUpdateView)
+                    ClientsDetailView, ClientsUpdateView, delete_client)
 
 app_name = 'app_clients'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', ClientsCreateView.as_view(), name='clients_create'),
     path('detail/<int:pk>/', ClientsDetailView.as_view(), name='clients_detail'),
     path('update/<int:pk>/', ClientsUpdateView.as_view(), name='clients_update'),
+    path('delete/<int:pk>/', delete_client, name='clients_delete'),
 ]
