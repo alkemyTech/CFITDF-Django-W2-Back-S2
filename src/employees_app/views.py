3f280a2756701c7f1955efcdddab829201407f5d
Emanuel-L <elucietti@gmail.com>
2025-06-03 19:43:55 -0300

    ema-cambios en app_empleados

--- src/employees_app/views.py
+++ src/employees_app/views.py
@@ -1,24 +1,8 @@
-from django.views.generic import CreateView, ListView, UpdateView, View, DetailView
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView, ListView, UpdateView,  DetailView
 from django.urls import reverse_lazy
 from .models import Employee
 from .forms import EmployeeForm
-
-class EmployeeListView(ListView):
-    model = Employee
-    template_name = 'employees_app/employee_list.html'
-    context_object_name = 'employees'
-
-def get_queryset(self):
-        return Employee.objects.filter(activo=True)
-
-class EmployeeListActiveView(ListView):
-    model = Employee
-    template_name = "employee_app/employees_list_activo.html"
-    context_object_name = "employees"
-
-    def get_queryset(self):
-        return Employee.objects.filter(is_active=True)
 
 
 class EmployeeCreateView(CreateView):
@@ -28,11 +12,27 @@ class EmployeeCreateView(CreateView):
     success_url = reverse_lazy('employees_app:employee_list')
 
 
+class EmployeeDetailView(DetailView):
+    model = Employee
+    template_name = 'employees_app/employee_detail.html'
+    context_object_name = 'employee'
+    
+
+class EmployeeListView(ListView):
+    model = Employee
+    template_name = 'employees_app/employee_list.html'
+    context_object_name = 'employees'
+    
+    def get_queryset(self):
+        return Employee.objects.filter(is_active=True)
+    
+
 class EmployeeUpdateView(UpdateView):
     model = Employee
     form_class = EmployeeForm
     template_name = 'employees_app/employee_form.html'
-    success_url = reverse_lazy('employees_app:employee_list_activo')
+    success_url = reverse_lazy('employees_app:employee_list_activo')    
+
 
 class EmployeeDeactivateView(UpdateView):
     model = Employee
@@ -41,10 +41,14 @@ class EmployeeDeactivateView(UpdateView):
     success_url = reverse_lazy('employees_app:employee_list_activo')
 
     def form_valid(self, form):
-        form.instance.active = False
+        form.instance.is_active = False
         return super().form_valid(form)
 
-class EmployeeDetailView(DetailView):
+
+class EmployeeListActiveView(ListView):
     model = Employee
-    template_name = 'employees_app/employee_detail.html'
-    context_object_name = 'employee'
+    template_name = "employee_app/employees_list_activo.html"
+    context_object_name = "employees"
+
+    def get_queryset(self):
+        return Employee.objects.filter(is_active=True)  
